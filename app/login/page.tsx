c8aa4b6fda9acd7bb8c7710d29ca306642edaed3
Aditi <jha.aditi21@gmail.com>
2024-08-11 20:58:27 -0400

    login UI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,24 @@
 import { login, signup } from './actions';
+import './LoginPage.css';
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="login-container">
+      <form className="login-form">
+        <h2>Login or Signup</h2>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input id="email" name="email" type="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input id="password" name="password" type="password" required />
+        </div>
+        <div className="button-group">
+          <button onClick={login}>Log in</button>
+          <button onClick={signup}>Sign up</button>
+        </div>
+      </form>
+    </div>
   );
 }
