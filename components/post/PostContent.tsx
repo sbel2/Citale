1d151635b9a3b5bdbaa4a056f85bacb8ccac2c01
anthua20 <tsehou26@bu.edu>
2025-03-04 13:17:58 -0500

    displaying likes on the comment screen

--- components/post/PostContent.tsx
+++ components/post/PostContent.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Linkify from 'react-linkify';
 import { Post } from "@/app/lib/types";
 import styles from "@/components/postComponent.module.css";
 import Image from 'next/image';
+import { supabase } from '@/app/lib/definitions';
 import { IoIosMore } from 'react-icons/io';
 import { useRouter } from "next/navigation";
 import { comment } from "postcss";
@@ -25,6 +26,9 @@ const PostContent: React.FC<PostContentProps> = ({ post, comments, deleteComment
   const address = post.mapUrl;
   const [openMenu, setOpenMenu] = useState<number | null>(null);
   const [showConfirm, setShowConfirm] = useState<number | null>(null);
+  const [showComments, setShowComments] = useState(true); // State to toggle comments visibility
+  const [likeDetails, setLikeDetails] = useState<{ id: string; username: string; avatar_url: string; liked_at: string }[]>([]);
+  const [likingCount, setLikingCount] = useState<number>(0)
   const router = useRouter();
 
   function isValidUrl(href: string): boolean {
@@ -35,6 +39,11 @@ const PostContent: React.FC<PostContentProps> = ({ post, comments, deleteComment
       return false;
     }
   }
+  useEffect(() => {
+    if (post.post_id) {
+      handleDisplayLikings();
+    }
+  }, [post.post_id]);
 
   const linkDecorator = (href: string, text: string, key: number): React.ReactNode => {
     if (!isValidUrl(href)) {
@@ -54,6 +63,46 @@ const PostContent: React.FC<PostContentProps> = ({ post, comments, deleteComment
     }
     toggleLike(comment.id);
     };
+
+  const handleDisplayLikings = async () => {
+    const { data, error } = await supabase
+      .from('likes')
+      .select('post_id, user_id, liked_at')
+      .eq('post_id', post.post_id);
+  
+    if (error) {
+      console.error('Error fetching likes:', error.message);
+      return;
+    }
+  
+    if (data && data.length > 0) {
+      const userDetailsPromises = data.map(async (like) => {
+        const { data: userData, error: userError } = await supabase
+          .from('profiles')
+          .select('id, username, avatar_url')
+          .eq('id', like.user_id)
+          .single();
+  
+        if (userError) {
+          console.error('Error fetching user:', userError.message);
+          return null;
+        }
+        return { ...userData, liked_at: like.liked_at }; // Include liked_at in the response
+      });
+  
+      const userDetails = await Promise.all(userDetailsPromises);
+  
+      // Filter out null values and ensure correct typing
+      const validUserDetails = userDetails.filter(
+        (user): user is { id: string; username: string; avatar_url: string; liked_at: string } =>
+          user !== null
+      );
+  
+      setLikeDetails(validUserDetails);
+    } else {
+      setLikeDetails([]); // No likes found
+    }
+  };
 
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
@@ -106,115 +155,181 @@ const PostContent: React.FC<PostContentProps> = ({ post, comments, deleteComment
 
       <div className='border-t border-gray-200 mt-8'></div>
 
-      <div className="mt-4 mb-48 md:mb-20">
-        {comments.map((comment) => {
-          const profile = comment.profiles || {};
-          return (
-            <div key={comment.id} className="mb-1 p-2 bg-white flex items-start">
-              <Image
-                src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/profile-pic/${profile.avatar_url || "avatar.png"}`}
-                alt="Profile"
-                width={40}
-                height={40}
-                className="rounded-full mr-3"
-              />
-              <div>
-                <p className="font-semibold">{profile.username || "Unknown User"}</p>
-                <p>{comment.content}</p>
-                <p className="text-xs text-gray-500 mt-1">{formatDate(comment.comment_at)}</p>
-                <button className="flex items-center p-1 pt-2 pr-2 gap-1" onClick={() => handleCommentLike(comment)}>
-                  {userLikes[comment.id] ? (
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15" color="red" fill="red">
-                      <path d="M19.4626 3.99415C16.7809 2.34923 14.4404 3.01211 13.0344 4.06801C12.4578 4.50096 12.1696 4.71743 12 4.71743C11.8304 4.71743 11.5422 4.50096 10.9656 4.06801C9.55962 3.01211 7.21909 2.34923 4.53744 3.99415C1.01807 6.15294 0.221721 13.2749 8.33953 19.2834C9.88572 20.4278 10.6588 21 12 21C13.3412 21 14.1143 20.4278 15.6605 19.2834C23.7783 13.2749 22.9819 6.15294 19.4626 3.99415Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                    </svg>
-                  ) : (
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15" color="#000000" fill="none">
-                      <path d="M19.4626 3.99415C16.7809 2.34923 14.4404 3.01211 13.0344 4.06801C12.4578 4.50096 12.1696 4.71743 12 4.71743C11.8304 4.71743 11.5422 4.50096 10.9656 4.06801C9.55962 3.01211 7.21909 2.34923 4.53744 3.99415C1.01807 6.15294 0.221721 13.2749 8.33953 19.2834C9.88572 20.4278 10.6588 21 12 21C13.3412 21 14.1143 20.4278 15.6605 19.2834C23.7783 13.2749 22.9819 6.15294 19.4626 3.99415Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                    </svg>
-                  )}
-                  <span className="text-xs inline-block w-4 text-center">{likes[comment.id] || 0}</span>
-                </button>
-              </div>
+      {/* Button Wrapper for Comments and Likes */}
+      <div className="flex items-center justify-center mt-4">
+        <button
+          className={`p-4 text-sm w-full flex justify-center items-center focus:outline-none transition-all rounded-lg relative ${
+            showComments ? '' : 'text-gray-300'
+          }`}
+          onClick={() => setShowComments(true)}
+        >
+          Comments
+          {showComments && (
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-20 border-t-4 border-black"></div>
+          )}
+        </button>
+        <button
+          className={`p-4 text-sm w-full flex justify-center items-center focus:outline-none transition-all rounded-lg relative ${
+            !showComments ? '' : 'text-gray-300'
+          }`}
+          onClick={() => setShowComments(false)}
+        >
+          Likes
+          {!showComments && (
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-20 border-t-4 border-black"></div>
+          )}
+        </button>
+      </div>
 
-              <div className="relative ml-auto">
-                {comment.user_id === userId && (
-                  <button
-                    onClick={() => setOpenMenu(openMenu === comment.id ? null : comment.id)}
-                    className="ml-auto text-gray-600 hover:text-gray-800"
-                  >
-                    <IoIosMore size={20} />
+      {/* Comments Section */}
+      {showComments && (
+        <div className="mt-4 mb-48 md:mb-20">
+          {comments.map((comment) => {
+            const profile = comment.profiles || {};
+            return (
+              <div key={comment.id} className="mb-1 p-2 bg-white flex items-start">
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/profile-pic/${profile.avatar_url || "avatar.png"}`}
+                  alt="Profile"
+                  width={40}
+                  height={40}
+                  className="rounded-full mr-3"
+                />
+                <div>
+                  <p className="font-semibold">{profile.username || "Unknown User"}</p>
+                  <p>{comment.content}</p>
+                  <p className="text-xs text-gray-500 mt-1">{formatDate(comment.comment_at)}</p>
+                  <button className="flex items-center p-1 pt-2 pr-2 gap-1" onClick={() => handleCommentLike(comment)}>
+                    {userLikes[comment.id] ? (
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15" color="red" fill="red">
+                        <path d="M19.4626 3.99415C16.7809 2.34923 14.4404 3.01211 13.0344 4.06801C12.4578 4.50096 12.1696 4.71743 12 4.71743C11.8304 4.71743 11.5422 4.50096 10.9656 4.06801C9.55962 3.01211 7.21909 2.34923 4.53744 3.99415C1.01807 6.15294 0.221721 13.2749 8.33953 19.2834C9.88572 20.4278 10.6588 21 12 21C13.3412 21 14.1143 20.4278 15.6605 19.2834C23.7783 13.2749 22.9819 6.15294 19.4626 3.99415Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+                      </svg>
+                    ) : (
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15" color="#000000" fill="none">
+                        <path d="M19.4626 3.99415C16.7809 2.34923 14.4404 3.01211 13.0344 4.06801C12.4578 4.50096 12.1696 4.71743 12 4.71743C11.8304 4.71743 11.5422 4.50096 10.9656 4.06801C9.55962 3.01211 7.21909 2.34923 4.53744 3.99415C1.01807 6.15294 0.221721 13.2749 8.33953 19.2834C9.88572 20.4278 10.6588 21 12 21C13.3412 21 14.1143 20.4278 15.6605 19.2834C23.7783 13.2749 22.9819 6.15294 19.4626 3.99415Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+                      </svg>
+                    )}
+                    <span className="text-xs inline-block w-4 text-center">{likes[comment.id] || 0}</span>
                   </button>
-                )}
+                </div>
 
-                {openMenu === comment.id && (
-                  <div className="absolute right-0 mt-1 w-32 bg-white shadow-lg border rounded z-20">
+                <div className="relative ml-auto">
+                  {comment.user_id === userId && (
                     <button
-                      className="w-full px-4 py-2 text-left text-red-600 hover:bg-gray-100"
-                      onClick={() => {
-                        setOpenMenu(null);
-                        setShowConfirm(comment.id);
-                      }}
+                      onClick={() => setOpenMenu(openMenu === comment.id ? null : comment.id)}
+                      className="ml-auto text-gray-600 hover:text-gray-800"
                     >
-                      Delete
+                      <IoIosMore size={20} />
                     </button>
-                  </div>
-                )}
-              </div>
-              {showConfirm === comment.id && (
-                <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
-                  <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-                    <p className="text-base mb-6">Are you sure you want to delete this comment?</p>
-                    <div className="flex justify-center gap-6">
+                  )}
+
+                  {openMenu === comment.id && (
+                    <div className="absolute right-0 mt-1 w-32 bg-white shadow-lg border rounded z-20">
                       <button
-                        className="bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded"
-                        onClick={() => setShowConfirm(null)}
-                      >
-                        Cancel
-                      </button>
-                      <button
-                        className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded"
+                        className="w-full px-4 py-2 text-left text-red-600 hover:bg-gray-100"
                         onClick={() => {
-                          deleteComment(comment.id);
-                          setShowConfirm(null);
+                          setOpenMenu(null);
+                          setShowConfirm(comment.id);
                         }}
                       >
-                        Yes, Delete
+                        Delete
                       </button>
                     </div>
-                  </div>
-                </div>
-              )}
-            </div>
-          );
-        })}
-      </div>
-      {showLoginPopup && (
-                  <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
-                      <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-                      <div className="flex justify-center mb-3">
-                          <Image src="/citale_header.svg" alt="Citale Logo" width={100} height={60} priority />
-                      </div>
-                      <p className="text-sm text-gray-600 mb-6">
-                          We are so glad you like Citale! <br /><br />
-                          Please sign in or sign up to interact with the community.
-                      </p>
-                      <div className="flex justify-center gap-6">
-                          <button
-                          className="bg-[#fd0000] hover:bg-[#fd0000] text-white px-4 py-2 rounded mr-2"
-                          onClick={() => router.push('/log-in')}
-                          >
-                          Log in
-                          </button>
-                          <button
-                          className="bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded"
-                          onClick={() => setShowLoginPopup(false)}
-                          >
-                          Cancel
-                          </button>
-                      </div>
-                      </div>
-                  </div>
                   )}
+                </div>
+                {showConfirm === comment.id && (
+                  <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
+                    <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+                      <p className="text-base mb-6">Are you sure you want to delete this comment?</p>
+                      <div className="flex justify-center gap-6">
+                        <button
+                          className="bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded"
+                          onClick={() => setShowConfirm(null)}
+                        >
+                          Cancel
+                        </button>
+                        <button
+                          className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded"
+                          onClick={() => {
+                            deleteComment(comment.id);
+                            setShowConfirm(null);
+                          }}
+                        >
+                          Yes, Delete
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      )}
+
+      {/* Likes Section */}
+      {!showComments && (
+  <div className="mt-4 mb-48 md:mb-20">
+    {likeDetails.length > 0 ? (
+      <ul>
+        {likeDetails.map((user) => (
+          <li key={user.id} className="mb-1 p-2 bg-white flex items-center justify-between">
+            <div className="flex items-center space-x-2">
+              <button
+                className="flex items-center space-x-2"
+                onClick={() => router.push(`/account/profile/${user.id}`)}
+              >
+                <img
+                  src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/profile-pic/${user.avatar_url}`}
+                  alt="profile"
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                />
+                <p className="text-sm">
+                  <span className="font-semibold">{user.username}</span> liked
+                </p>
+              </button>
+            </div>
+            <p className="text-xs text-gray-500">
+              {formatDate(user.liked_at)} {/* Add a timestamp if available */}
+            </p>
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <p className="text-sm text-gray-500">No likes yet.</p>
+    )}
+  </div>
+)}
+
+      {showLoginPopup && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+            <div className="flex justify-center mb-3">
+                <Image src="/citale_header.svg" alt="Citale Logo" width={100} height={60} priority />
+            </div>
+            <p className="text-sm text-gray-600 mb-6">
+                We are so glad you like Citale! <br /><br />
+                Please sign in or sign up to interact with the community.
+            </p>
+            <div className="flex justify-center gap-6">
+                <button
+                className="bg-[#fd0000] hover:bg-[#fd0000] text-white px-4 py-2 rounded mr-2"
+                onClick={() => router.push('/log-in')}
+                >
+                Log in
+                </button>
+                <button
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded"
+                onClick={() => setShowLoginPopup(false)}
+                >
+                Cancel
+                </button>
+            </div>
+            </div>
+        </div>
+      )}
     </div>
   );
 };
