abaea95bd0358fdc722a1b24d21a07e2927d98ab
annaandmandy <annaandtommy1004@gmail.com>
2025-03-10 14:38:51 -0400

    message button

---
+++ app/(chat)/inbox/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import React, { useState, useEffect, useRef } from 'react';
+import { supabase } from '@/app/lib/definitions'; // Ensure correct import
+import { useAuth } from 'app/context/AuthContext';
+
+export default function Inbox(){
+    
+}
