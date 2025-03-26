ac6eb789d202d5e72f5cbf15aba5755d530c7467
annaandmandy <annaandtommy1004@gmail.com>
2025-03-26 16:58:37 -0400

    post display

--- app/(postaccess)/[post]/[id]/layout.tsx
+++ app/(postaccess)/[post]/[id]/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import '../../../globals.css';
+import Toolbar from "@/components/toolbar"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,8 +10,17 @@ export default function PostLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className={`${inter.className} bg-gray-951`}>
-      <main>{children}</main>
+    <div className={`${inter.className} bg-white flex h-screen`}>
+      {/* Toolbar - hidden on small screens, visible on large screens */}
+      <div className="md:block w-64">
+        <Toolbar />
+      </div>
+
+      {/* Main content area */}
+      <main className="flex-1 flex justify-center">
+        {/* Center content on the right side of the screen */}
+        <div className="w-full">{children}</div>
+      </main>
     </div>
   );
 }
